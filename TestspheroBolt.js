ee8d337168d6ed146ed3ba0f291b9048a631b032
Gavin Butcher <gavinbutcher.20@gmail.com>
2023-02-13 07:37:18 -0500

    Added delays and changed colors/sounds

--- TestspheroBolt.js
+++ TestspheroBolt.js
@@ -2,7 +2,11 @@
 //Date:2.6.2023
 //Project: Sphero Bolt
 
-
+/** 
+*Hello World Practice startProgram
+* LED turn to blue 
+*foward for 2 seconds at 60 speed
+* /
 //Hello World Practice Program
 async function helloWorld() {
 	await speak("Hello World", true);
@@ -21,6 +25,28 @@ async function ninetyDegree() {
 	await roll((getHeading() + 0), 60, 4); // Heading 0 degrees at 60 speed for 4 seconds
 	await delay(1);
 	await roll((getHeading() + 90), 60, 4); // heading 90 degrees at 60 speed for 4 seconds
+}
+
+async function startProgram() {
+
+	setMainLed({ r: 0, g: 0, b: 255 });
+
+	await speak("Hello Square", true);
+
+	await delay(1);
+
+	for (var _i1 = 0; _i1 < 4; _i1++) {
+
+		setMainLed(getRandomColor());
+
+		await Sound.Game.Coin.play(true);
+
+		await roll((getHeading() + 90), 60, 1);
+
+		await delay(1);
+
+	}
+
 }
 
 /**
